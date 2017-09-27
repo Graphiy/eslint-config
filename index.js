c2cf8f1ac08095e3c517a88a51c3e38c07b134db
Dmitra <adrian@dmitra.com>
2017-09-27 08:41:39 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
+    // array1[i] = array2[j] should be possible
+    'prefer-destructuring': ['error', { AssignmentExpression: { array: false } }],
     // Automatic semicolon insertion saves time and symbols
     semi: ['error', 'never'],
     'func-names': 0,
@@ -9,6 +11,7 @@ module.exports = {
     'vars-on-top': 0,
     'comma-dangle': ['error', 'always-multiline'],
     'space-before-function-paren': ['error', 'always'],
+    // clean after debugging
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
     // it is necessary to document what is passed to callback
     'no-unused-vars': ['error', { args: 'none' }],
@@ -16,8 +19,12 @@ module.exports = {
     'no-undefined': 0,
     // no need in empty parentheses after self-explanatory new tagKeysword
     'new-parens': 0,
+    // let's deprecate C++ than
+    'no-plusplus': 0,
     'no-loop-func': 0,
+    // mutational functions are a great feature of JS
     'no-param-reassign': 0,
+    // linting is a bad way to fix language
     'no-continue': 0,
     // naming convention for private methods and params
     'no-underscore-dangle': 0,
